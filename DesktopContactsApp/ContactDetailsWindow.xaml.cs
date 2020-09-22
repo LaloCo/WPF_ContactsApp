e3d4fed72893c7d9822b453a4d79af1b172d4be1
Eduardo Rosas <lalorosas@outlook.com>
2020-09-22 16:04:10 -0500

    Deleting from table

---
+++ DesktopContactsApp/ContactDetailsWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using DesktopContactsApp.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for ContactDetailsWindow.xaml
+    /// </summary>
+    public partial class ContactDetailsWindow : Window
+    {
+        Contact selectedContact;
+        public ContactDetailsWindow(Contact selectedContact)
+        {
+            InitializeComponent();
+
+            this.selectedContact = selectedContact;
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Contact>();
+                conn.Update(selectedContact);
+            }
+
+            Close();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Contact>();
+                conn.Delete(selectedContact);
+            }
+
+            Close();
+        }
+    }
+}
